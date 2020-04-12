3323ddca3fcd001064022946db8fb8da53229a64
tyh <756467256@qq.com>
2020-04-12 15:58:01 +0800

    {YunHan}{feat}--{SpringBoot}{JDBC}:add ini.sql

---
+++ src/ini.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `student` (
+  `name` varchar(4) NOT NULL,
+  `gender` varchar(6) DEFAULT NULL,
+  `class_name` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
